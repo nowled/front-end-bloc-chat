735788765a04e5fab714729c37226fadaa47e24f
Charles Peterson <nowledster@gmail.com>
2017-05-17 18:46:40 +0300

    2 List Chat Rooms

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,10 @@
 (function() {
-    function HomeCtrl() {
+    function HomeCtrl(Room) {
+
+        this.rooms = Room.all;
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['Room', HomeCtrl]);
 })();
